2bc83a82ff438e495534f7b076b804110dfb70f3
Erayaltilar <eray.altilar@hotmail.com>
2024-06-19 23:10:41 +0300

    Add update Profile

--- app/src/main/java/com/example/eray_altilar_final/presentation/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/eray_altilar_final/presentation/ui/profile/ProfileScreen.kt
@@ -3,8 +3,10 @@ package com.example.eray_altilar_final.presentation.ui.profile
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,13 +14,21 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.eray_altilar_final.core.Resource
+import com.example.eray_altilar_final.domain.model.usermodel.UserUpdateRequest
 
 @Composable
 fun ProfileScreen(viewModel: ProfileViewModel = hiltViewModel()) {
 
     val state by viewModel.user.collectAsState()
+
+    val userUpdateRequest = UserUpdateRequest(
+        email = "ErayFALAN MAIL",
+        firstName = "ERAY",
+        lastName = "ALTILAR",
+    )
 
     when (state) {
         is Resource.Loading -> {
@@ -28,24 +38,37 @@ fun ProfileScreen(viewModel: ProfileViewModel = hiltViewModel()) {
         }
 
         is Resource.Success -> {
-            state.data?.let { Text(text = it.email) }
+            state.data?.let {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp),
+                ) {
+                    LazyColumn {
+                        item {
+                            LazyRow {
+                                item {
+                                    Text(text = it.username)
+                                }
+                            }
+                            Text(text = it.email)
+                            Text(text = it.firstName)
+                            Text(text = it.lastName)
+
+                            Button(onClick = {
+                                viewModel.updateUser(it.id, userUpdateRequest)
+                            }) {
+                                Text(text = "Update")
+                            }
+                        }
+                    }
+                }
+            }
 
         }
 
         is Resource.Error -> {
             Text(text = state.message.orEmpty())
-        }
-    }
-    Column(modifier = Modifier.fillMaxSize()) {
-        LazyColumn {
-            item {
-                LazyRow {
-                    item {
-                        Text(text = "Eray's")
-                        Text(text = "Falann")
-                    }
-                }
-            }
         }
     }
 }
--- app/src/main/java/com/example/eray_altilar_final/presentation/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/eray_altilar_final/presentation/ui/profile/ProfileViewModel.kt
@@ -4,29 +4,72 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.eray_altilar_final.core.Resource
-import com.example.eray_altilar_final.core.SharedPreferencesManager.getUserId
+import com.example.eray_altilar_final.core.SharedPreferencesManager.getToken
 import com.example.eray_altilar_final.domain.model.usermodel.User
-import com.example.eray_altilar_final.domain.model.usermodel.Users
-import com.example.eray_altilar_final.domain.usecase.GetUserByIdUseCase
-import com.example.eray_altilar_final.domain.usecase.GetUsersUseCase
+import com.example.eray_altilar_final.domain.model.usermodel.UserUpdateRequest
+import com.example.eray_altilar_final.domain.usecase.user.GetUserByIdUseCase
+import com.example.eray_altilar_final.domain.usecase.user.GetUserByTokenUseCase
+import com.example.eray_altilar_final.domain.usecase.user.UpdateUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
     private val getUserByIdUseCase: GetUserByIdUseCase,
+    private val getUserByTokenUseCase: GetUserByTokenUseCase,
+    private val updateUserUseCase: UpdateUserUseCase,
 ) : ViewModel() {
 
     private val _user = MutableStateFlow<Resource<User>>(Resource.Loading())
     val user: StateFlow<Resource<User>> get() = _user
 
     init {
-        val userId = getUserId()
-        getUser(userId)
-        Log.d("UserID",  "init: $userId")
+        val token = getToken()
+        getUserByToken(token)
+
+
+    }
+
+    fun updateUser(id: Long, userUpdateRequest: UserUpdateRequest) {
+        updateUserUseCase(id, userUpdateRequest).onEach {
+            when (it) {
+                is Resource.Loading -> {
+                    _user.value = Resource.Loading()
+                }
+
+                is Resource.Success -> {
+                    _user.value = Resource.Success(it.data!!)
+                }
+
+                is Resource.Error -> {
+                    _user.value = Resource.Error(it.message)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun getUserByToken(token: String?) {
+        getUserByTokenUseCase(token!!).onEach {
+            when (it) {
+                is Resource.Loading -> {
+                    _user.value = Resource.Loading()
+                }
+
+                is Resource.Success -> {
+                    _user.value = Resource.Success(it.data!!)
+                    Log.d("CurrentUserEmail", "getUser: ${it.data}")
+                }
+
+                is Resource.Error -> {
+                    _user.value = Resource.Error(it.message)
+                }
+            }
+        }.launchIn(viewModelScope)
     }
 
     private fun getUser(userId: Long) {
@@ -34,6 +77,7 @@ class ProfileViewModel @Inject constructor(
             getUserByIdUseCase(userId)
                 .collect { result ->
                     _user.value = result
+                    Log.d("User", "getUser: ${result.data?.email}")
                 }
         }
     }
