0d6e2520c9a9900f8b837db76d14d7cad5526246
Erayaltilar <eray.altilar@hotmail.com>
2024-06-20 23:33:23 +0300

    Create new mapper

--- app/src/main/java/com/example/eray_altilar_final/data/mapper/CartMapper.kt
+++ app/src/main/java/com/example/eray_altilar_final/data/mapper/CartMapper.kt
@@ -2,9 +2,21 @@ package com.example.eray_altilar_final.data.mapper
 
 import com.example.eray_altilar_final.data.local.entity.CartEntity
 import com.example.eray_altilar_final.domain.model.cartmodel.Cart
+import com.example.eray_altilar_final.domain.model.productmodel.Product
 
 fun CartEntity.toCart(): Cart {
     return Cart(
+        cartId = cartId,
+        productId = productId,
+        name = name,
+        thumbnail = thumbnail,
+        price = price,
+        userId = userId
+    )
+}
+
+fun Cart.toCartEntity(): CartEntity {
+    return CartEntity(
         cartId = cartId,
         productId = productId,
         name = name,
