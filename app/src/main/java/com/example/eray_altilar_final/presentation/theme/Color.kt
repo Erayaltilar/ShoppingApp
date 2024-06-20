a910d84ce2cddf2ce5c9429f5705684c0a54d4a1
Erayaltilar <eray.altilar@hotmail.com>
2024-06-20 23:29:04 +0300

    Add UI constants

--- app/src/main/java/com/example/eray_altilar_final/presentation/theme/Color.kt
+++ app/src/main/java/com/example/eray_altilar_final/presentation/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.example.eray_altilar_final.presentation.theme
 
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+@Stable
+val Color.Companion.FF2196F3: Color
+    get() = Color(0xFF2196F3)
+
+@Stable
+val Color.Companion.FF21CBF3: Color
+    get() = Color(0xFF21CBF3)
+
+@Stable
+val Color.Companion.FFF44336: Color
+    get() = Color(0xFFF44336)
