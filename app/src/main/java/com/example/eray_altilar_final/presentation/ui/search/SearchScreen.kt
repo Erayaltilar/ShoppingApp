8c4de94677b6a7ea2bc8d25c8b883a936adbd83f
Erayaltilar <eray.altilar@hotmail.com>
2024-06-21 20:20:58 +0300

    Add Search logic to SearchScreen

--- app/src/main/java/com/example/eray_altilar_final/presentation/ui/search/SearchScreen.kt
+++ app/src/main/java/com/example/eray_altilar_final/presentation/ui/search/SearchScreen.kt
@@ -1,12 +1,82 @@
 package com.example.eray_altilar_final.presentation.ui.search
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.eray_altilar_final.domain.model.productmodel.Product
+import com.example.eray_altilar_final.presentation.theme.Dimen
 
 @Composable
-fun SearchScreen() {
-    Column() {
-        Text(text = "Search")
+fun SearchScreen(viewModel: SearchViewModel = hiltViewModel()) {
+
+    val uiState by viewModel.uiState.collectAsState()
+    val context = LocalContext.current
+
+    with(uiState) {
+        if (loadingState) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+            return@with
+        }
+        if (isHaveError) {
+            Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show()
+            return@with
+        }
+        if (isSuccess) {
+        }
+
+        SearchScreenUI(
+            products = products,
+        )
+    }
+}
+
+@Composable
+fun SearchScreenUI(
+    products: List<Product>
+) {
+    var query by remember { mutableStateOf("") }
+    val filteredProducts = products.filter {
+        it.title?.contains(query, ignoreCase = true) == true
+    }
+
+    Column {
+        OutlinedTextField(
+            value = query,
+            onValueChange = { query = it },
+            label = { Text("Search") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(Dimen.spacing_m1)
+        )
+        LazyColumn {
+            items(filteredProducts.size) { productIndex ->
+                Text(
+                    text = filteredProducts[productIndex].title ?: "",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(Dimen.spacing_xs)
+                )
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/eray_altilar_final/presentation/ui/search/SearchViewModel.kt
@@ -0,0 +1,77 @@
+package com.example.eray_altilar_final.presentation.ui.search
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.eray_altilar_final.core.Resource
+import com.example.eray_altilar_final.domain.model.productmodel.Product
+import com.example.eray_altilar_final.domain.usecase.product.remote.GetProductsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    val getProductsUseCase: GetProductsUseCase,
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SearchScreenUIState())
+    val uiState: StateFlow<SearchScreenUIState> = _uiState.asStateFlow()
+
+    private var currentPage = 0
+    private val pageSize = 30
+
+    init {
+        getProducts()
+    }
+
+    private fun getProducts() {
+        getProductsUseCase(pageSize, currentPage).onEach {
+            when (it) {
+                is Resource.Loading -> {
+                    _uiState.update { state ->
+                        state.copy(
+                            loadingState = true,
+                            isHaveError = false,
+                            isSuccess = false,
+                        )
+                    }
+                }
+
+                is Resource.Success -> {
+                    _uiState.update { state ->
+                        state.copy(
+                            loadingState = false,
+                            isHaveError = false,
+                            isSuccess = true,
+                            errorMessage = "",
+                            products = it.data?.products ?: emptyList()
+                        )
+                    }
+                }
+
+                is Resource.Error -> {
+                    _uiState.update { state ->
+                        state.copy(
+                            loadingState = false,
+                            isHaveError = true,
+                            isSuccess = false,
+                        )
+                    }
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    data class SearchScreenUIState(
+        val loadingState: Boolean = false,
+        val isHaveError: Boolean = false,
+        val isSuccess: Boolean = false,
+        val errorMessage: String = "",
+        val products: List<Product> = emptyList()
+    )
+}
